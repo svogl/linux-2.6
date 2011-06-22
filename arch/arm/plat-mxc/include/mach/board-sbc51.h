e4e54d34fd8ff6fe7cade84fc601c64bb75df3f8
Simon Vogl <simon.vogl@beko.at>
2011-06-22 15:04:47 +0200

    added utility drivers for gps, gsm blocks on COMM daughter board.

---
+++ arch/arm/plat-mxc/include/mach/board-sbc51.h
@@ -0,0 +1,15 @@
+#ifndef __BOARD_SBC51_H__
+#define __BOARD_SBC51_H__
+
+struct blt_gps_platform_data {
+	char *reg;
+	int uV;
+};
+
+struct blt_gsm_platform_data {
+	void (*power) (int on);
+	void (*reset) (void);
+};
+
+
+#endif
